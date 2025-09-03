56e72abdaf350e5d430d4e0a845dd19651cc751e
Balasubramaniam <bala.23m0405@gmail.com>
2025-09-03 22:34:33 +0530

    Create 1d_array.c med

---
+++ 1d_array.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+    int n=0,sum;
+    scanf("%d",&n);
+    
+    int arr[n];
+    for(int i=0;i<n;i++){
+        scanf("%d",&arr[i]);
+    }
+    
+    for(int i=0;i<n;i++){
+        sum+=arr[i];
+    }
+    printf("%d",sum);
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */    
+    return 0;
+}
